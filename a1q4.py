d3e1ba56b489284e483beb2cf033b49052dc71be
Nikita000kumari <144161710+Nikita000kumari@users.noreply.github.com>
2024-06-19 00:12:37 +0530

    Add files via upload

---
+++ a1q4.py
@@ -0,0 +1,20 @@
+# Write a program that asks the user for their name and then prints a greeting message.
+str_a = input("enter your name:")
+print("hi! good morning :)",str_a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
