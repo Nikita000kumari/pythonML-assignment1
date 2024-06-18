fcf96a3d83d3cebb7dc7176ac4da4762a4fbca84
Nikita000kumari <144161710+Nikita000kumari@users.noreply.github.com>
2024-06-18 23:44:06 +0530

    Add files via upload

---
+++ a1q2.py
@@ -0,0 +1,21 @@
+# Write a python program that checks whether a given number is even or odd.
+
+a = int(input("enter a number to check whether it is even or odd:"))
+if(a % 2 == 0):
+    print("the number is even .")
+else:
+    print("the number is odd.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
