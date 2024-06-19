0d7bcf434dac505d0ca3b13c2cd4518328ccf664
Nikita000kumari <144161710+Nikita000kumari@users.noreply.github.com>
2024-06-19 07:17:17 +0530

    Add files via upload

---
+++ a1q7.py
@@ -0,0 +1,12 @@
+# Write a python program that takes a string as input and returns its length.
+str_a = input("enter the string : ")
+b = len(str_a)
+print(f"the length of the string is ,{b}")
+
+
+
+
+
+
+
+
