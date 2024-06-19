dcef9c5e573b48ff9cc92e7ea6cbae13489949da
Nikita000kumari <144161710+Nikita000kumari@users.noreply.github.com>
2024-06-19 08:20:42 +0530

    Add files via upload

---
+++ a1q10.py
@@ -0,0 +1,6 @@
+# Write a python program that converts a given string to uppercase.
+input_string = input(" enter the string:")
+upper_text = input_string.upper()
+print(f" the upper string of the given string is {upper_text}")
+
+
