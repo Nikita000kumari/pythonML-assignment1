31dc9c9f3a2909032925072cfc8644b4a92d81cd
Nikita000kumari <144161710+Nikita000kumari@users.noreply.github.com>
2024-06-19 07:22:34 +0530

    Add files via upload

---
+++ a1q8.py
@@ -0,0 +1,17 @@
+# Write a python program that concatenates two strings and returns the result.
+str_a = input("enter a string:")
+str_b = input("enter another string:")
+c = str_a + str_b
+print(f"the concatenates of two string is :{c}")
+
+
+
+
+
+
+
+
+
+
+
+
