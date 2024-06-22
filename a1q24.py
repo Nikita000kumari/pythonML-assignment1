bd8bf3b28b0301a5691e8fae25484b1492c256ec
Nikita000kumari <144161710+Nikita000kumari@users.noreply.github.com>
2024-06-22 17:46:24 +0530

    Add files via upload

---
+++ a1q24.py
@@ -0,0 +1,23 @@
+# Write a program that acts as a simple calculator. It should take two
+# numbers and an operator (+, -, *, /) as input and print the result.
+c = int(input("enter a number :"))
+d = int(input("enter a number :"))
+a = c + d
+print("the addition is :",a)
+y = c - d
+print("the subtraction is :",y)
+s = c * d
+print(" the multiplication is :",s)
+w = c % d
+print(" the division is : ",w)
+
+
+
+
+
+
+
+
+
+
+
